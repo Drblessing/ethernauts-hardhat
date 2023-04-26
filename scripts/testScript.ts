833a84585c544fb0bb75a55f4aba684b7925d994
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-04-26 09:43:31 -0700

    fix package

---
+++ scripts/testScript.ts
@@ -0,0 +1,27 @@
+function greet(person: string, date: Date) {
+  return 'Hello, ' + person;
+}
+
+greet('hey', new Date());
+
+interface Circle {
+  kind: 'circle';
+  radius: number;
+}
+
+interface Square {
+  kind: 'square';
+  sideLength: number;
+}
+
+type Shape = Circle | Square;
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case 'circle':
+      return Math.PI * shape.radius ** 2;
+
+    case 'square':
+      return shape.sideLength ** 2;
+  }
+}
